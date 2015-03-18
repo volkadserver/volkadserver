6df87ef40acac10380e4bce054b4fdc698ed81eb
Nathan Hagen <nathan@aggronn.com>
2015-03-18 18:20:27 -0400

    Implement basic browser name targeting

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 var koa = require('koa');
+var _ = require('lodash');
+var ua = require('useragent');
 
 var app = koa();
+var flights = [
+  { 
+    flightName: 'test',
+    family: 'Chrome',
+
+    creatives: [
+      { creativeName: 'creativetest', creativeContent: 'testcontent' }
+    ]
+  }
+];
+
 
 app.use(function *() {
-  this.body = JSON.stringify(this.request.header, null, 2);
+  var start = new Date();
+  var criteria = 
+    _.pick(ua.parse(this.request.headers['user-agent']), 'family');
+  var res = _.find(flights, criteria);
+  if(res && res.creatives && res.creatives[0])
+    this.body = res.creatives[0].creativeContent;
+  else
+    this.body = 'House ad';
+  var end = new Date() - start;
+  console.log('took: ', end);
 });
 
 app.listen(8081);
